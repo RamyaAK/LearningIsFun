010f7b102713d0c7e9a7a4bd97526866025e2d2b
RamyaK <ext_rakulkarni@falabella.cl>
2021-01-15 22:06:24 +0530

    adding javaPrograms practiced today on variables and ForEachLoop

---
+++ src/java_programs/ForEachLoop_Example.java
@@ -0,0 +1,14 @@
+package java_programs;
+
+public class ForEachLoop_Example {
+
+        public static void main(String args[])
+        {
+            char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+            // foreach loop
+            for (char item: vowels)
+            {
+                System.out.println(item);
+            }
+        }
+}
